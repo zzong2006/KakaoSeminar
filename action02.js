567e9be7a682290e639dac5040066af8c759e8f2
Bong <zzong2006@gmail.com>
2021-07-27 15:30:23 +0900

    Update action02.js

--- action02.js
+++ action02.js
@@ -10,7 +10,7 @@ const octokit = new Octokit({
 
 
 octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/labels', {
-    owner: 'EntTangunGHEC',
+    owner: 'zzong2006',
     repo: 'KakaoSeminar',
     issue_number: core.getInput('issue_number'),
     labels: [
